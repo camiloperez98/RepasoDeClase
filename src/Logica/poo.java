578f7c4cc6a845c98b028caca3180b97ad239ae1
camilo perez <jcperez6249@misena.edu.co>
2022-10-26 19:20:46 -0500

    Initial commit

--- src/Logica/poo.java
+++ src/Logica/poo.java
@@ -5,5 +5,16 @@ public class poo {
 
         Alumno alu1=new Alumno();
         Alumno alu2=new Alumno(12, "camilo","perez");
+        System.out.println("la id del alumno 2: "+alu2.getId());
+
+        alu1.setId(5);
+        alu1.setNombre("kelly");
+        alu1.setApellido("perez");
+
+        System.out.println("-------------------------------");
+
+        System.out.println("el nombre del alumno 1 es: "+ alu1.getNombre());
+        System.out.println("apellido: "+alu1.getApellido());
+        System.out.println("ID: "+alu1.getId());
     }
 }
